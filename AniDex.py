120859bf66da9c61eeb781dd797a47a15c0e515d
Golumpa <Golumpa@users.noreply.github.com>
2017-08-13 05:40:06 +0100

    Update to work with the release API

--- AniDex.py
+++ AniDex.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
 
 #-----------------------------------------------------------------------------
-# Copyright (c) 2017, Golumpa.
+#                 Copyright (c) 2017, Golumpa.
 #
-# Distributed under the terms of the MIT License.
-#
-# The full license is in the file LICENSE, distributed with this software.
+#           _          _ ____            ____              
+#          / \   _ __ (_)  _ \  _____  _|  _ \ _   _       
+#         / _ \ | '_ \| | | | |/ _ \ \/ / |_) | | | |      
+#        / ___ \| | | | | |_| |  __/>  <|  __/| |_| |      
+#  ____ /_/   \_\_|_|_|_|____/ \___/_/\_\_|    \__, |      
+# | __ ) _   _   / ___| ___ | |_   _ _ __ ___  |___/  __ _ 
+# |  _ \| | | | | |  _ / _ \| | | | | '_ ` _ \| '_ \ / _` |
+# | |_) | |_| | | |_| | (_) | | |_| | | | | | | |_) | (_| |
+# |____/ \__, |  \____|\___/|_|\__,_|_| |_| |_| .__/ \__,_|
+#        |___/                                |_|          
 #-----------------------------------------------------------------------------
 
 import sys, getopt
 import requests
+import json
 from pprint import pprint
 
 def main(argv):
@@ -17,22 +25,23 @@ def main(argv):
 	apiurl = "https://anidex.info/api/" # URL to API, this shouldn't need to change, but if you want to anyway you can change it here or use the -u arg in command line.
 	apikey = "myapitoken" # Use the same API every time? enter it here so you dont have to enter it every time with the -a arg.
 	torrentfile = "" # File to upload, just setting the variable. Use the -f arg to set this in command line.
-	subcatid = "3" # 3 = Anime Dub, See https://i.golu.mp/OO9zv for API catagory IDs. Use the -c arg to set this in command line.
+	subcatid = "1" # 1 = Anime Sub, See https://i.golu.mp/OO9zv for API catagory IDs. Use the -c arg to set this in command line.
 	language = "1" # 1 = English, See https://i.golu.mp/CJ7tr for language IDs. Use the -l arg to set this in command line.
-	send = "true" # Apparently its needed in the API. I belive true is needed for it to be posted on AniDex, use --nosend to turn this to false.
+	debug = "1" # Apparently its needed in the API. I belive true is needed for it to be posted on AniDex, use --nosend to turn this to false.
 
 	# Optional Config
 	torrentname = "" # Defualt name for the torrent. Use the -n arg to set this in the command line.
-	description = "Uploaded with AniDex remote by Golumpa" # default description for the torrent use the -d arg to set this in command line.
+	description = "Uploaded with AniDexPy by Golumpa" # default description for the torrent use the -d arg to set this in command line.
 	group = "0" # 0 = NO group, set this ID or use the -g arg if you want to release to a group.
 	batch = "0" # 0 = NOT a batch, set to "1" or use --batch in command line if it is a batch.
 	hentai = "0" # 0 = NOT hentai, set to "1" or use --hentai in command line if it is hentai.
 	reencode = "0" # 0 = NOT a re-encode,  set to "1" or use --reencode in command line if it is a re-encode.
 	private = "0" # 0 = NOT private, set to "1" or use --private in commandline if you DO NOT want to publish this torrent publicly.
+	ttapi = "0" # 0 = NOT to send to TokyoTosho via API. The TT API needs to be set in your AniDex account setting beforehand.
 
 	helptext = """#=#=#=#=#=#=#=#=#=#\nAniDex API Uploader\n#=#=#=#=#=#=#=#=#=#
 
-  USAGE: AniDex.py -a <apikey> -f <torrentfile> -c <category> -l <language> -n <torrentname> -d <description> -g <group_id> -u <apiurl> --batch --hentai --reencode --private --nosend
+  USAGE: AniDex.py -a <apikey> -f <torrentfile> -c <category> -l <language> -n <torrentname> -d <description> -g <group_id> -u <apiurl> --tt --batch --hentai --reencode --private --debug
 
   -a <apikey>\n  REQUIRED: The API Key you get from your AniDex Account Settings under the "Upload API" section. https://anidex.info/?page=settings\n
   -f <torrentfile>\n  REQUIRED: The path to the .torrent file you wish to upload. "always best to do absolute paths personally" eg. "/home/golumpa/Boku no Pico.torrent"\n
@@ -42,14 +51,15 @@ def main(argv):
   -d <description>\n  OPTIONAL: The description of the torrent, try to keep it informal as everyone hates blank descriptions that say or show nothing about the release.\n
   -g <group_id>\n  OPTIONAL: The Group ID that you wish to post this torrent under.\n
   -u <apiurl>\n  OPTIONAL: The URL of the AniDex API, you shouldnt need to change this value but on the offchance you can.\n
+  --tt\n  OPTIONAL: Option to send to the TokyoTosho API. The TT API key needs to be already set in your AniDex account.\n
   --batch\n  OPTIONAL: Option to set this torrent as a batch with multiple episodes.\n
   --hentai\n  OPTIONAL: Option to set this torrent to a hentai release so its hidden to the people that dont want to see nsfw content.\n
   --reencode\n  OPTIONAL: Option to set this release to a re-encode release, basically where you run the original through something like handbrake to get a different quality.\n
   --private\n  OPTIONAL: Option to set the torrent to private so only you AND your group members can view it.\n
-  --nosend\n  OPTIONAL: Not sure if this works but I assume this will make it so it doenst actually submit the torrent to AniDex. Not sure why you would want it though...\n"""
+  --debug\n  OPTIONAL: This will send the torrent and return all the variables recieved by AniDex. This will NOT actually upload the torrent and should only be used for testing.\n"""
 
 	try:
-		opts, args = getopt.getopt(argv,"ha:f:n:c:l:d:g:u:",["file=","apiurl=","apikey=","cat=","name=","group=","lang=","desc=","batch","hentai","nosend","reencode","private"])
+		opts, args = getopt.getopt(argv,"ha:f:n:c:l:d:g:u:",["file=","apiurl=","apikey=","cat=","name=","group=","lang=","desc=","batch","hentai","debug","reencode","private","tt"])
 	except getopt.GetoptError:
 		print(helptext)
 		sys.exit(2)
@@ -60,9 +70,9 @@ def main(argv):
 		elif opt in ("-f", "--file"):
 			torrentfile = arg
 		elif opt in ("-u", "--apiurl"):
-			url = arg
+			apiurl = arg
 		elif opt in ("-a", "--apikey"):
-			apiToken = arg
+			apikey = arg
 		elif opt in ("-c", "--cat"):
 			subcatid = arg
 		elif opt in ("-n", "--name"):
@@ -77,15 +87,17 @@ def main(argv):
 			batch = "1"
 		elif opt in ("--hentai"):
 			hentai = "1"
-		elif opt in ("--nosend"):
-			send = "false"
+		elif opt in ("--debug"):
+			debug = "1"
 		elif opt in ("--reencode"):
 			reencode = "1"
 		elif opt in ("--private"):
 			private = "1"
+		elif opt in ("--tt"):
+			ttapi = "1"
+
 	data = {
 		'subcat_id':subcatid,
-		'file':torrentfile,
 		'torrent_name':torrentname,
 		'group_id':group,
 		'lang_id':language,
@@ -95,8 +107,11 @@ def main(argv):
 		'reencode':reencode,
 		'private':private,
 		'api_key':apikey,
-		'send':send
+		'debug':debug,
+		'tt_api':ttapi
 	}
+
+
 	if torrentfile != "":
 		response = requests.post(apiurl,
 			data=data,
@@ -104,7 +119,7 @@ def main(argv):
 	else:
 		response = requests.post(apiurl,
 			data=data)
-	pprint(response.text)
+	print(response.text)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
